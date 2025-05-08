94f058705a04e0e96b5fd78117bcaf5b2fea41df
lourenco01 <eaglesaxon@gmail.com>
2025-05-08 18:52:18 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => <h1>Olá, LúminaLivros!</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
