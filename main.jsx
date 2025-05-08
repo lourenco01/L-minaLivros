4169d96bfa3fed2682f2528dd089adf22ff216e6
lourenco01 <eaglesaxon@gmail.com>
2025-05-08 17:35:45 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function App() {
+  return <h1>Bem-vindo à LúminaLivros</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
